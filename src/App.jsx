3c9c52efa01ad7fb067aee7901c220c4e787b8f4
Orlando L Dominici <orlandoldominici@Orlandos-Air.home>
2023-03-02 23:34:14 -0800

    added basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import DatePicker from "./components/DatePicker"
 
 function App() {
   return (
-    <main className="">
-      <DatePicker />
+    <main className="h-screen flex justify-center items-center m-6">
+      <div>
+        <h1 className="text-center text-2xl">Appt Tracker</h1>
+        <DatePicker />
+      </div>
     </main>
   )
 }
